f36737713fcefe501f1a6c0d075f0a99f648025a
alexgap <alnik.gap@gmail.com>
2017-02-05 21:47:51 +0300

    Guessing game
    implementation of GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+		this.min = 0;
+		this.max = 0;
+		this.attempt = 0;
+	}
 
     setRange(min, max) {
-
+		this.min = min;
+		this.max = max;
     }
 
     guess() {
-
+		this.attempt = Math.ceil((this.min + this.max) / 2);
+		return this.attempt;
     }
 
     lower() {
-
+		this.max = this.attempt;
+		this.attempt = Math.ceil((this.min + this.max) / 2);
     }
 
     greater() {
-
+		this.min = this.attempt;
+		this.attempt = Math.ceil((this.min + this.max) / 2);
     }
 }
 
